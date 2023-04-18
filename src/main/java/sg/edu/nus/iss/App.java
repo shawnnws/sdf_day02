b5300001786540e3db38c3f9b0bc6bbdd01bdbeb
shawnnws <shawnngweisong@gmail.com>
2023-04-18 11:09:02 +0800

    Add ArrayList

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,10 +1,25 @@
 package sg.edu.nus.iss;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        MountainBike mb = new MountainBike(5, 10, 20);
-        System.out.println(mb.toString());
+        MountainBike mb1 = new MountainBike(5, 10, 20);
+        System.out.println(mb1.toString());
+
+        MountainBike mb2 = new MountainBike(1, 2, 3);
+        MountainBike mb3 = new MountainBike(4, 5, 6);
+
+        List<MountainBike> bicycles = new ArrayList<MountainBike>();
+        bicycles.add(mb1);
+        bicycles.add(mb2);
+        bicycles.add(mb3);
+
+        for (int i = 0; i < bicycles.size(); i++) {
+            System.out.println("Bike " + (i + 1) + ":" + bicycles.get(i).toString());
+        }
     }
 }
