21c010cec6d884daa5a694fa2b2e259185b46c7f
shawnnws <shawnngweisong@gmail.com>
2023-04-18 16:12:01 +0800

    LinkedList example

---
+++ src/main/java/sg/edu/nus/iss/LinkedListExample.java
@@ -0,0 +1,28 @@
+package sg.edu.nus.iss;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    
+    public void LinkedList() {
+
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        linkedList.add("Heng Swee Kiat");
+        linkedList.add("Leong Mun Wai");
+        linkedList.add("Pritam Singh");
+        linkedList.add("Gerald Giam");
+
+        for (String itm : linkedList) {
+            System.out.println(itm);
+        }
+
+        linkedList.remove("Leong Mun Wai");
+        System.out.println(linkedList);
+
+        linkedList.addFirst("Lee Hsien Loong");
+        linkedList.addLast("Lee Kuan Yew");
+        System.out.println(linkedList);
+        
+    }   
+}
